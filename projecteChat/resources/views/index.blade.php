338297183d7016798db7c932485c377d16071332
Alejandro Novoa <anovoamontolio@iesesteveterradas.cat>
2018-02-12 20:17:34 +0100

    Sprint 1 casi acabado!

--- projecteChat/resources/views/index.blade.php
+++ projecteChat/resources/views/index.blade.php
@@ -5,11 +5,19 @@
 @extends('layouts.master')
 
 @section('content')
-	@if (session('status'))
-        <div class="alert alert-success">
-            {{ session('status') }}
-        </div>
-    @endif
-    inivial
+	<div class="text-left">
+    <p>Bienvenidos a la página web de Cornellà de Llobregat, en esta pagina web podreis:</p>
+    <ul>
+    <li>Chatear con gente de vuestra zona.</li>
+    <li>Poner vuestras reclamaciones sobre si alguna zona de Cornellà necessita un arreglo o si por otras razones.</li>
+    <li>Un foro para poder chatear sobre los problemas de Cornellà o novedades.</li>
+    <li>Hacer intercanvios entre vosotros.</li>
+    </ul>
+
+	</div>
+    
+    
+    
+    
 
 @stop
--- projecteChat/resources/views/layouts/breadCrumbs.blade.php
+++ projecteChat/resources/views/layouts/breadCrumbs.blade.php
@@ -1,6 +1,28 @@
 <ul class="breadcrumb">
-	<li><a href="#">Home</a></li>
-	<li><a href="#">Products</a></li>
-	<li class="active">Accessories</li>
+	@if (Request::is('/'))
+		<li class="active">Inicio</li>
+	@else
+		<li><a href="{{ url('/') }}">Inicio</a></li>
+	@endif
+
+	@if (Request::is('chatroom'))
+		<li class="active">Chatroom</li>
+	@elseif (Request::is('denuncia'))
+		<li class="active">Crear denuncias</li>
+	@elseif (Request::is('denuncia/show*'))
+		@if (Request::is('denuncia/show'))
+			<li class="active">Ver denuncias</li>
+		@else
+			<li><a href="{{ url('denuncia/show') }}">Ver denuncias</a></li>
+		@endif
+
+		@if (Request::is('denuncia/show/*'))
+			<li class="active">Ver informacion denuncia</li>
+		@endif
+	@elseif (Request::is('foro'))
+		<li class="active">Foro</li>
+	@elseif (Request::is('intercanvios'))
+		<li class="active">Intercanvios</li>
+	@endif
 </ul>
 
