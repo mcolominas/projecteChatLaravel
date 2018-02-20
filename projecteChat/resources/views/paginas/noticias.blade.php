0d2a331030c484d6b390ea87fbaa7fef0970b958
Alejandro Novoa <anovoamontolio@iesesteveterradas.cat>
2018-02-20 20:24:31 +0100

    Final apuntito

--- projecteChat/resources/views/paginas/noticias.blade.php
+++ projecteChat/resources/views/paginas/noticias.blade.php
@@ -28,37 +28,48 @@
 
 <div class="row text-left">
 	<div class="col-md-offset-1 col-md-10">
-	<div class="row text-center">
-		<div class="col-md-12"><legend class="text-center header" id="">
-		@if (isset($tipo))
-			Noticias
-		@endif
-		
 
-		</legend></div>
-	</div>
+    	@foreach( $noticiasImportantes as $noticia )
 
-	@foreach( $noticias as $noticia )
-
-	<div class="row marginTop">
-    	<div class="col-md-3"><img src='{{ ($noticia->imagen == NULL) ? URL::asset("/img/imageEmpty.png") : URL::asset("$noticia->imagen") }}' class="img-responsive"></div>
-        <div class="col-md-9">
-        	<div class="row">
-        		<div class="col-md-12">
-        			<h3>{{ $noticia->titulo }}</h3>
-        		</div>
-        	</div>
-        	<div class="row">
-        		<div class="col-md-12">
-        			{{ $noticia->mensaje }}
-        		</div>
-        	</div>
+        <div class="row marginTop">
+            <div class="col-md-3"><img src='{{ ($noticia->imagen == NULL) ? URL::asset("/img/imageEmpty.png") : URL::asset("$noticia->imagen") }}' class="img-responsive"></div>
+            <div class="col-md-9">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h3>{{ $noticia->titulo }}</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        {{ $noticia->mensaje }}
+                    </div>
+                </div>
+            </div>
         </div>
-  	</div>
 
-  	@endforeach
+        @endforeach
 
-	</div>
+        @foreach( $noticias as $noticia )
+
+        <div class="row marginTop">
+            <div class="col-md-3"><img src='{{ ($noticia->imagen == NULL) ? URL::asset("/img/imageEmpty.png") : URL::asset("$noticia->imagen") }}' class="img-responsive"></div>
+            <div class="col-md-9">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h3>{{ $noticia->titulo }}</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        {{ $noticia->mensaje }}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        @endforeach
+
+    	</div>
 	</div>
 
 @stop
