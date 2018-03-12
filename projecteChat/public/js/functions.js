ce97a6b6435c752404e9877294d575f4efda0a1a
alejandrooo96 <alejandrooo96@gmail.com>
2018-03-12 20:25:06 +0100

    Examen Leandro

---
+++ projecteChat/public/js/functions.js
@@ -0,0 +1,42 @@
+jQuery(function(){ 
+
+	//Función que cambia el color de fondo y guarda el valor en la cookie
+	function cambiacolor(elem, valor, name, css){ 		//Recibe el color de fondo 'bgcolor'
+		elem.css(css,valor);		//Cambia el color de fondo de la web a 'bgcolor'
+		Cookies.set(name,valor);					//Crea una cookie llamada 'colorCookie' con el color elegido 'bgcolor'
+		
+	}
+	
+	//Al cargar la página se pone de color de fondo el valor que hay en la cookie 
+	jQuery('.navbar-inverse').css('background-color',Cookies.get('menu'));
+	jQuery('body').css('background-color',Cookies.get('fondo'));
+	jQuery('body').css('font-size',Cookies.get('letra'));
+
+	//Al clicar sobre la imagen obtenemos el color del la id del input y llamamos la función 'cambiacolor' con el color seleccionado
+	jQuery('#clicar').click(function(e){
+		e.preventDefault();
+		cambiacolor(jQuery('.navbar-inverse'), $("#menu2").val(), "menu", "background-color");
+		
+	});
+
+	jQuery('#clicar2').click(function(e){
+		e.preventDefault();
+		cambiacolor(jQuery('body'), $("#menu3").val(), "fondo", "background-color");
+		
+	});
+
+	jQuery('#clicar3').click(function(e){
+		e.preventDefault();
+		cambiacolor(jQuery('#letra'), $("#menu4").val(), "letra", "font-size");
+		
+	});
+	
+	//Al clicar sobre el botón de eliminar cookies, borramos la cookie	
+	jQuery("#eliminaCookie").click(function(){
+		Cookies.remove('menu');
+		Cookies.remove('fondo');
+		Cookies.remove('letra');
+		location.reload();
+	});
+		
+});
--- projecteChat/resources/views/index.blade.php
+++ projecteChat/resources/views/index.blade.php
@@ -1,11 +1,16 @@
+@section('head')
+    @parent
+@stop
+
 @section('title')
     Projecte Vota - Inicio
+
 @stop
 
 @extends('layouts.master')
 
 @section('content')
-	<div class="text-left">
+	<div id="letra" class="text-left">
     <p>Bienvenidos a la página web de Cornellà de Llobregat, en esta pagina web podreis:</p>
     <ul>
     <li>Chatear con gente de vuestra zona.</li>
@@ -15,9 +20,47 @@
     </ul>
 
 	</div>
-    
-    
-    
-    
 
+    <!-- Trigger the modal with a button -->
+    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Abrir estetica</button>
+
+    <!-- Modal -->
+    <div id="myModal" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title">Cambiar color</h4>
+          </div>
+
+          <div class="col-md-12 col-xs-12">
+          <div class="col-md-4 col-xs-4"> 
+                <div>Menu  
+                    <input id="menu2" name="text">
+                    <button id="clicar" type="button" class="btn btn-primary">APLICAR</button>
+                </div> 
+            </div>
+            <div class="col-md-4 col-xs-4"> 
+                <div>Fondo  
+                    <input id="menu3" name="text">
+                    <button id="clicar2" type="button" class="btn btn-primary">APLICAR</button>
+                </div> 
+            </div>
+            <div class="col-md-4 col-xs-4"> 
+                 <div>Tamaño Letra  
+                    <input id="menu4" name="text">
+                    <button id="clicar3" type="button" class="btn btn-primary">APLICAR</button>
+                </div> 
+            </div>
+            </div>
+
+          <div class="modal-footer">
+            <button id="eliminaCookie" type="button" class="btn btn-default" data-dismiss="modal">Valores por defecto</button>
+            <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+          </div>
+        </div>
+
+      </div>
+    </div>
 @stop
